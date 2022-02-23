63fa815b7840bb0b4ccf947d93500b647b06ef9e
Eric Costa <ericdevcosta@users.noreply.github.com>
2022-02-22 23:38:08 -0300

    Create treasure.py
    added treasure.py

---
+++ game/cast/treasure.py
@@ -0,0 +1,31 @@
+from game.cast.actor import Actor
+
+class Artifact(Actor):
+    """
+    Two items to be displayed on screen. Rock and Gems. 
+
+    The responsibility of an this class is to create rocks and gems.
+
+    Attributes:
+        _message (string): A short description about the item.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+
+    def get_message(self):
+        """Gets the artifact's message.
+        
+        Returns:
+            string: The message.
+        """
+        return self._message
+    
+    def set_message(self, message):
+        """Updates the message to the given one.
+        
+        Args:
+            message (string): The given message.
+        """
+        self._message = message
